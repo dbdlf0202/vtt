658dc320381b7df414f871c8d3b371755cb2df45
김유일 <dbdlf0202@gmail.com>
2025-12-01 14:13:38 +0900

    Auto update 2025-12-01 14:13:38.65

--- auto_push.bat
+++ auto_push.bat
@@ -1,14 +1,21 @@
 @echo off
-:: VTT 자막 폴더로 이동
+:: 1. Git 저장소 경로로 이동
 cd /d "C:\Users\CJPC\Desktop\VTT 자막 아카이브"
 
-:: Git 변경 사항 추가
+:: 2. Git 저장소인지 확인
+if not exist ".git" (
+    echo Error: Git repository not found!
+    pause
+    exit /b
+)
+
+:: 3. 변경 사항 추가
 git add .
 
-:: 커밋 (자동 메시지: 날짜와 시간)
+:: 4. 커밋 (자동 메시지: 날짜와 시간)
 git commit -m "Auto update %date% %time%"
 
-:: GitHub로 push
+:: 5. GitHub로 push
 git push
 
 :: 완료 메시지
